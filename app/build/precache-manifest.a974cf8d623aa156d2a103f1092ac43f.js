0e29e7b3d57915d5ae4cc262d4d517092c8b75c9
Marvin ROGER <dev@marvinroger.fr>
2019-09-11 15:46:02 +0200

    docs: add word document and built app

---
+++ app/build/precache-manifest.a974cf8d623aa156d2a103f1092ac43f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a42a70f02784fa950c7a3e12e3edc51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb0f5881f9991f8a69b1",
+    "url": "./static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "34d9be4709c78d2294e6",
+    "url": "./static/js/2.70f6b952.chunk.js"
+  },
+  {
+    "revision": "cb0f5881f9991f8a69b1",
+    "url": "./static/js/main.1c8e0edb.chunk.js"
+  },
+  {
+    "revision": "7d717205f6269d3d4371",
+    "url": "./static/js/runtime~main.2019f22e.js"
+  },
+  {
+    "revision": "a78800c791ec0c0617b54fadcf3929eb",
+    "url": "./static/media/logo.a78800c7.svg"
+  }
+]);
